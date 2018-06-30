9839fc185d3ec1f286d4d9101bf01a740fd36887
wrathchild317 <angelrivera4213@gmail.com>
2018-06-29 17:01:06 -0700

    Changed the paths file to hold the new mlabs mongodb connection string and the database name, also updated .gitignore with the dist/ directory given that it is not necessary

--- dist/bin/paths.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-exports.default = {
-	mongodb: 'mongodb://localhost:27017',
-	natural_disasters_db_str: "natural_disasters_db"
-};
--- utils/paths.js
+++ utils/paths.js
@@ -1,6 +1,6 @@
 export default {
-	mongodb: "mongodb://localhost:27017",
-	natural_disasters_db_str: "natural_disasters_db",
+	mongodb: "mongodb://AbyssWatcherOwner:AbyssWatcher185@ds123171.mlab.com:23171/heroku_xcvxfpp4",
+	natural_disasters_db_str: "heroku_xcvxfpp4",
 	earthquakes: "/earthquakes",
 	tsunamis: "/tsunamis",
 	volcanoes: "/volcanoes",
