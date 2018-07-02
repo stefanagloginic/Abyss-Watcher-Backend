2797cecc5e35878433a7e9799f67887f71a7049e
wrathchild317 <angelrivera4213@gmail.com>
2018-07-02 02:27:35 -0700

    change for heroku, access build of the client

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use('/abyss-watcher/v1', v1Routes);
 /*--------------Integrate client----------------*/
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
-	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 	// Handle React routing, return all requests to React app
 	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+		res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 	});
 }
 
