988974c51520c6c391f793ff1d41a2445093ce09
stefanagloginic <stefana.gloginic@gmail.com>
2018-07-09 10:34:38 -0700

    adding changes..still broken

--- client/src/Components/Legend.js
+++ client/src/Components/Legend.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
+
+import * as d3 from 'd3'
+import { select } from 'd3-selection'
 //--------------CSS----------------------
 import '../Stylesheets/Legend.css';
 //--------------Components----------------------
@@ -16,17 +19,55 @@ class Legend extends Component {
 		this.plottedND = plottedND;
 	}
 
-	render() {
+	componentDidMount = () => {
+		this.createLegend();
+	}
+
+	createLegend = () => {
+		const node = this.node;
+		var width = window.innerWidth;
+		var height = window.innerHeight;
 		this.getProps();
 
-		var colorLegends = (this.plottedND) ? this.plottedND.map(function(nd) {
-			return <ColorLegend name={ nd.type } />
-		}) : [];
+		select(node)
+			.append("g")
+			.attr("class", "legend_group");
+
+		var legends = select(node)
+			.select("g");
+		
+		legends
+			.data(this.plottedND)
+			.enter()
+			.append("g")
+			.attr("class", "legend");
+
+		legends.append('rect')
+		    .attr('width', 10)
+		    .attr('height', 10);
+		    // .style('fill', function(d) {
+		    // 	return color(d.name);
+		    // })
+		legends.append('svg:text')
+		    .text(function(d) {
+		        return d.type;
+		    })
+		    .attr("class", "legend_text");
+;
+	}
+
+	render() {
+		// var colorLegends = (this.plottedND) ? this.plottedND.map(function(nd) {
+		// 	return <ColorLegend name={ nd.type } />
+		// }) : [];
 
 		return (
-			<div className="legend_wrapper">
-				{colorLegends}
-			</div>
+			<svg 
+				className="legend_wrapper" 
+				ref={node => this.node = node} 
+				width={ '15%'} 
+				height={ '15%' } 
+				/>
 		);
 	}
 }
